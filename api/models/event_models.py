67147e223f0559852298beff95fa8ac5c8e150bf
TallTalha <talhaburakaydin.official@gmail.com>
2025-08-08 13:40:35 +0300

    Geliştirme: API uygulaması için dosya yapısı oluşturuldu.

---
+++ api/models/event_models.py
@@ -0,0 +1,6 @@
+# click-event-big-data-app/api/models/event_models.py
+"""
+Bu modül, API tarafından kabul edilen veri yapılarını Pydantic modelleri
+olarak tanımlar. Bu modeller, gelen verinin otomatik olarak doğrulanmasını,
+dönüştürülmesini ve dokümante edilmesini sağlar.
+"""
